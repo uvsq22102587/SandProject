ae8b9dd4735019702eaadc932d42ef233c3f41be
Arthur ViNCENS <vincensarthur@gmail.com>
2022-02-11 10:31:05 +0100

    Changement layout

--- V2/tas_de_sable.py
+++ V2/tas_de_sable.py
@@ -1,4 +1,6 @@
 ###################################
-# groupe : Benjamin Prehaud , Arthur Vincens
+# groupe LDDBI 1 
+# Benjamin PREHAUD 
+# Arthur VINCENS
 # https://github.com/uvsq22102587/SandProject
 ###################################
