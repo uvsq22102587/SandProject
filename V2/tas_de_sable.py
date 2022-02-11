81410ee3f8547fd6be407270a25d0a79d6c1e2be
Benjamin PREHAUD <benjamin.prehaud@ens.uvsq.fr>
2022-02-11 11:59:05 +0100

    tableaux

--- V2/tas_de_sable.py
+++ V2/tas_de_sable.py
@@ -1,6 +1,22 @@
 ###################################
-# groupe LDDBI 1 
-# Benjamin PREHAUD 
+# groupe LDDBI 1
+# Benjamin PREHAUD
 # Arthur VINCENS
 # https://github.com/uvsq22102587/SandProject
 ###################################
+
+# import des librairies
+from logging import root
+import tkinter as tk
+
+
+
+root = tk.Tk()
+root.title("tas de sable")
+cTableau = tk.Canvas(root, height=500, width=500, bg="white")
+cTableau.grid()
+root.mainloop()
+
+
+
+
