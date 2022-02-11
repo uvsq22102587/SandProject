0adce45a26ab04fecd911646e1f569be4da9bb56
Arthur ViNCENS <vincensarthur@gmail.com>
2022-02-11 12:25:53 +0100

    jd

--- V2/tas_de_sable.py
+++ V2/tas_de_sable.py
@@ -1,6 +1,20 @@
 ###################################
-# groupe LDDBI 1 
-# Benjamin PREHAUD 
+# groupe LDDBI 1
+# Benjamin PREHAUD
 # Arthur VINCENS
 # https://github.com/uvsq22102587/SandProject
 ###################################
+
+
+def chooseConfig():
+    boperation.grid_remove()
+    bconfig.grid_remove()
+
+bconfig = tk.Button(root, text="Config", command = chooseConfig)
+bconfig.grid(column = 0, row = 0)
+
+boperation = tk.Button(root, text="Operation")
+boperation.grid(column = 0, row = 1)
+
+bsauvegarde = tk.Button(root, text="Sauvegarder la config")
+bsauvegarde.grid(column = 0, row = 2)
